def0b804f75735b66f8a82cc1692fa8711156bc3
Alun Ward <a.ward@eyedivision.com>
2023-02-14 13:36:05 +0000

    Change collections

--- js/main.js
+++ js/main.js
@@ -36,55 +36,56 @@
     });
 
     /* thumbnail switch on products */
-    let thumbnails = document.getElementsByClassName('thumbnail')
+    if(document.body.contains(document.getElementById('featured'))){
+
+      let thumbnails = document.getElementsByClassName('thumbnail')
+      
+      let activeImages = document.getElementsByClassName('active')
+
+      for (var i=0; i < thumbnails.length; i++){
+
+          thumbnails[i].addEventListener('click', function(){
+
+              console.log(activeImages)
+              
+              if (activeImages.length > 0){
+                  activeImages[0].classList.remove('active')
+              }
+              
+              this.classList.add('active')
+
+              document.getElementById('featured').src = this.src
+              let original = document.getElementById('featured').src
+              let result = original.replace(/100/g, 1200)
+
+              document.getElementById('featured').src = result
+              $('#featured').removeAttr('width').removeAttr('height');
+
+              document.getElementById('zoom').style.backgroundImage = "url('" + result + ")";
+
+          })
+      }
+
+      // magnify on zoom
+      const zoomtarget = document.getElementById('zoom')
+
+      zoomtarget.addEventListener('mousemove', (e) => {
+
+          let zoomer = e.currentTarget;
+
+          let offsetX = ''
+          let offsetY = ''
+          let x = ''
+          let y = ''
+          e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+          e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+          x = offsetX/zoomer.offsetWidth*100
+          y = offsetY/zoomer.offsetHeight*100
+          zoomer.style.backgroundPosition = x + '% ' + y + '%';
+
+      })
     
-    let activeImages = document.getElementsByClassName('active')
-
-    for (var i=0; i < thumbnails.length; i++){
-
-        thumbnails[i].addEventListener('click', function(){
-
-            console.log(activeImages)
-            
-            if (activeImages.length > 0){
-                activeImages[0].classList.remove('active')
-            }
-            
-            this.classList.add('active')
-
-            document.getElementById('featured').src = this.src
-            let original = document.getElementById('featured').src
-            let result = original.replace(/100/g, 1200)
-
-            document.getElementById('featured').src = result
-            $('#featured').removeAttr('width').removeAttr('height');
-
-            document.getElementById('zoom').style.backgroundImage = "url('" + result + ")";
-
-        })
-    }
-
-
-    // magnify on zoom
-    const zoomtarget = document.getElementById('zoom')
-
-    zoomtarget.addEventListener('mousemove', (e) => {
-
-        let zoomer = e.currentTarget;
-
-        let offsetX = ''
-        let offsetY = ''
-        let x = ''
-        let y = ''
-        e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-        e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-        x = offsetX/zoomer.offsetWidth*100
-        y = offsetY/zoomer.offsetHeight*100
-        zoomer.style.backgroundPosition = x + '% ' + y + '%';
-
-    })
-    
-    
+    } // end if element exists
 
   }); // end doc ready
 
